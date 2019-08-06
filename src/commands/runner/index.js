5c37a23288db80ab7e6be64dd37ac3abc78d15b0
nemanjan00 <nemanjan00@gmail.com>
2019-08-06 19:08:07 +0200

    Fix deps

--- src/commands/runner/index.js
+++ src/commands/runner/index.js
@@ -6,7 +6,9 @@ module.exports = (argv) => {
 	const options = curl2options(argv);
 
 	got(options).then(data => {
-		console.log(data);
+		console.log(data.statusCode);
+		console.log(data.headers);
+		console.log(data.body);
 	}).catch(error => {
 		console.error(error);
 	});
